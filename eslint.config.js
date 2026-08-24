07987e96d768719cc9caf183bda1ffd3938f6f9a
jia812328 <2713640542@qq.com>
2026-08-24 20:42:16 +0800

    添加 eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+// @noge/destrokenpreview
+
+const AngularEslint = require(
+  '@angular-eslint/builder-templates'
+);
+
+module.exports = [
+  ...AngularEslint('@angular-eslint/angular-eslint'),
+];
